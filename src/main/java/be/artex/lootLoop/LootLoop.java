377b5ce1dc3bbc456b2f2692726fc8263a926fd1
Artex154 <a.stockelynck12@outlook.com>
2025-05-09 22:53:16 +0200

    #2 - Added Join & Quit messages

--- src/main/java/be/artex/lootLoop/LootLoop.java
+++ src/main/java/be/artex/lootLoop/LootLoop.java
@@ -1,17 +1,13 @@
 package be.artex.lootLoop;
 
+import be.artex.lootLoop.listeners.PlayerConnectionEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LootLoop extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(new PlayerConnectionEvent(), this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
---
+++ src/main/java/be/artex/lootLoop/listeners/PlayerConnectionEvent.java
@@ -0,0 +1,27 @@
+package be.artex.lootLoop.listeners;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerConnectionEvent implements Listener {
+    @EventHandler
+    public void playerJoin(PlayerJoinEvent event) {
+        event.joinMessage(Component.text("[", NamedTextColor.DARK_GRAY)
+                .append(Component.text("-", TextColor.color(50, 210, 50)))
+                .append(Component.text("] ", NamedTextColor.DARK_GRAY))
+                .append(Component.text("Artex54")));
+    }
+
+    @EventHandler
+    public void playerQuit(PlayerQuitEvent event) {
+        event.quitMessage(Component.text("[", NamedTextColor.DARK_GRAY)
+                .append(Component.text("-", NamedTextColor.DARK_RED))
+                .append(Component.text("] ", NamedTextColor.DARK_GRAY))
+                .append(Component.text("Artex54")));
+    }
+}
