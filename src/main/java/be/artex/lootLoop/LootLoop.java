a48a27f2955007a91c3d8e8b298a911ff233f75e
Artex154 <a.stockelynck12@outlook.com>
2026-02-16 19:38:44 +0100

    registered the playerDeathEvent listener

--- src/main/java/be/artex/lootLoop/LootLoop.java
+++ src/main/java/be/artex/lootLoop/LootLoop.java
@@ -1,13 +1,8 @@
 package be.artex.lootLoop;
 
-import be.artex.lootLoop.api.Mineral;
 import be.artex.lootLoop.command.Sell;
 import be.artex.lootLoop.command.Test;
-import be.artex.lootLoop.listener.InventoryClick;
-import be.artex.lootLoop.listener.PlayerBlockEvent;
-import be.artex.lootLoop.listener.PlayerConnectionEvent;
-import be.artex.lootLoop.listener.PlayerInteract;
-import be.artex.lootLoop.mineral.Stone;
+import be.artex.lootLoop.listener.*;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -21,6 +16,7 @@ public final class LootLoop extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new PlayerBlockEvent(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerInteract(), this);
         Bukkit.getPluginManager().registerEvents(new InventoryClick(), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerDeathEvent(), this);
 
         getCommand("sell").setExecutor(new Sell());
         getCommand("test").setExecutor(new Test());
