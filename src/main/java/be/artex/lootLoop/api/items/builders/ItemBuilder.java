42895f927e5675290529e19ea66f61b737d9768f
Artex154 <a.stockelynck12@outlook.com>
2026-03-07 18:26:34 +0100

    Added mining speed

--- src/main/java/be/artex/lootLoop/api/items/builders/ItemBuilder.java
+++ src/main/java/be/artex/lootLoop/api/items/builders/ItemBuilder.java
@@ -1,9 +1,18 @@
 package be.artex.lootLoop.api.items.builders;
 
 import be.artex.lootLoop.api.items.ItemRarity;
+import be.artex.lootLoop.api.items.ToolProperties;
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.ItemAttributeModifiers;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.inventory.EquipmentSlotGroup;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
@@ -14,7 +23,9 @@ public class ItemBuilder {
     private final ItemStack stack;
     private final ItemRarity rarity;
     private Component name = Component.empty();
-    private List<Component> description = new ArrayList<>();
+    private final List<Component> description = new ArrayList<>();
+    private int miningSpeed = 0;
+    private int miningFortune = 0;
 
     public ItemBuilder(ItemStack stack, ItemRarity rarity) {
         this.stack = stack.clone();
@@ -43,22 +54,44 @@ public class ItemBuilder {
     }
 
     public ItemBuilder description(Component... description) {
-        List<Component> l = new ArrayList<>();
-        Collections.addAll(l, description);
+        Collections.addAll(this.description, description);
+        return this;
+    }
 
-        l.add(Component.empty());
-        l.add(rarity.getDisplayText().decoration(TextDecoration.ITALIC, false));
+    public ItemBuilder tool(ToolProperties properties) {
+        this.miningSpeed = properties.miningSpeed();
+        this.miningFortune = properties.miningFortune();
 
-        this.description = l;
+        this.stack.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+
+        if (this.miningSpeed != 0)
+            this.description.add(Component.text("+" + this.miningSpeed, NamedTextColor.GOLD).append(
+                    Component.text(" ᴍɪɴɪɴɢ sᴘᴇᴇᴅ.", NamedTextColor.GRAY)));
+
+        if (this.miningFortune != 0)
+            this.description.add(Component.text("+" + this.miningFortune, NamedTextColor.GOLD).append(
+                    Component.text(" ᴍɪɴɪɴɢ ғᴏʀᴛᴜɴᴇ.", NamedTextColor.GRAY)));
 
         return this;
     }
 
     public ItemStack build() {
+        AttributeModifier miningSpeedModifier = new AttributeModifier(
+                new NamespacedKey("lootloop", "mining_speed"),
+                this.miningSpeed,
+                AttributeModifier.Operation.ADD_NUMBER);
+
+        stack.setData(DataComponentTypes.ATTRIBUTE_MODIFIERS,
+                ItemAttributeModifiers.itemAttributes().addModifier(
+                        Attribute.BLOCK_BREAK_SPEED, miningSpeedModifier, EquipmentSlotGroup.MAINHAND));
+
         ItemStackBuilder builder = ItemStackBuilder.create(stack);
 
         if (!Component.empty().equals(this.name))
             builder.name(this.name);
+
+        this.description.add(Component.empty());
+        this.description.add(rarity.getDisplayText().decoration(TextDecoration.ITALIC, false));
 
         builder.lore(this.description);
 
